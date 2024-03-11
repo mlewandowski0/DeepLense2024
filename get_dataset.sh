36719dc88ebdaddefb93f828252af98e94de29f0
Maciej Lewandowski <lewandowski.maciej1702@gmail.com>
2024-03-11 15:52:30 +0000

    task 1 and corrected get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,12 +1,18 @@
 pip install gdown
+apt-get update
+apt-get upgrade -y
+apt-get install unzip
+
 gdown --fuzzy https://drive.google.com/file/d/1ZEyNMEO43u3qhJAwJeBZxFBEYc_pVYZQ/view
 gdown --fuzzy https://drive.google.com/file/d/1cJyPQzVOzsCZQctNBuHCqxHnOY7v7UiA/view
 
+unzip -q dataset.zip
+unzip -q Samples.zip
+
 mkdir Dataset
 mkdir Dataset/superresolution
-mv dataset.zip Dataset
-unzip Dataset/dataset.zip
-mv Samples.zip Dataset/superresolution
-unzip Dataset/superresolution/Samples.zip
-mv Samples Dataset
+
 mv dataset Dataset
+mv Samples Dataset/superresolution
+
+rm dataset.zip Samples.zip
